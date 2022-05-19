72c404a054762ba103a28abe28ebf375081b3e20
liva-nazarenko <105019479+liva-nazarenko@users.noreply.github.com>
2022-05-19 21:01:33 +0300

    homework1

--- src/main/java/homework/FirstClass.java
+++ src/main/java/homework/FirstClass.java
@@ -3,5 +3,33 @@ package homework;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("This is homework");
+
+        String valstsNosaukums = "Sanmarīno";
+        System.out.println("Valsts nosaukums: "+ valstsNosaukums);
+
+        int iedzivotajuSkaits = 33930;
+        System.out.println("Iedzīvotāju skaits: "+ iedzivotajuSkaits);
+
+        double platiba = 61.2;
+        System.out.println("Platība: "+ platiba + "km2");
+
+        String valstsGalvaspilseta = "Sanmarīno";
+        System.out.println("Galvaspilsēta: "+ valstsGalvaspilseta);
+
+        String oficialaValoda = "itāļu";
+        System.out.println("Valsts valoda: "+ oficialaValoda);
+
+        boolean eiropasSavienibasDalibvalsts = false;
+        System.out.println("ES dalībvalsts: "+ eiropasSavienibasDalibvalsts);
+
+        char valstsValuda = '€';
+        System.out.println("Valūta: "+ valstsValuda);
+        
+
+
+
+
+
+
     }
 }
