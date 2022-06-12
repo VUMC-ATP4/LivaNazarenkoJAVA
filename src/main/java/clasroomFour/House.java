4712b1cf10b97cf39ceb80997efd2e8accdb0be3
liva-nazarenko <105019479+liva-nazarenko@users.noreply.github.com>
2022-06-13 00:45:19 +0300

    Trissturis

---
+++ src/main/java/clasroomFour/House.java
@@ -0,0 +1,35 @@
+package clasroomFour;
+
+public class House {
+
+    String ielasNosaukums;
+    int majasNumurs;
+    int stavuSkaits;
+    int ieejuSkaits;
+    int istabuSkaits;
+    double platiba;
+    int cena;
+    boolean irStavvieta;
+    int skatijumuSkaits;
+
+    public double cenaKvadratmetra(){
+        return cena.cena/platiba;
+    }
+
+    public void printetAdresi(){
+        System.out.println("Mājas adrese ir: " + ielasNosaukums + " " + majasNumurs);
+    }
+
+    public void printetVisu() {
+        printetAdresi();
+        System.out.println("Māja: " +
+                "stavuSkaits=" + stavuSkaits +
+                ", ieejuSkaits=" + ieejuSkaits +
+                ", istabuSkaits=" + istabuSkaits +
+                ", platiba=" + platiba + "m^2" +
+                ", cena=" + cena +
+                ", irStavvieta=" + irStavvieta +
+                ", cena kvadrātmetrā ir " + cena.cena/platiba +
+                '}');
+    }
+}
