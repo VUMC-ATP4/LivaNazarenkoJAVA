8a380a1b84c317de76c26bb353927560ae3206ff
liva-nazarenko <105019479+liva-nazarenko@users.noreply.github.com>
2022-06-18 18:32:12 +0300

    classFive

---
+++ src/main/java/clasroomFive/inheritance/Dog.java
@@ -0,0 +1,13 @@
+package clasroomFive.inheritance;
+
+public class Dog extends Animal {
+
+    public Dog(int legCount,String name) {
+        super(legCount, name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Es esmu suns. Vau vau vau....");
+    }
+}
