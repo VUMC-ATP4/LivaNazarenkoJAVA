966b8d071421af11769c6b6c506e3604ba2128c6
liva-nazarenko <105019479+liva-nazarenko@users.noreply.github.com>
2022-06-18 17:10:13 +0300

    up

--- src/main/java/clasroomFour/House.java
+++ src/main/java/clasroomFour/House.java
@@ -1,33 +1,33 @@
-package clasroomFour;
-
-public class House {
-
-    String ielasNosaukums;
-    int majasNumurs;
-    int stavuSkaits;
-    int ieejuSkaits;
-    int istabuSkaits;
-    double platiba;
-    Cena cena;
-    boolean irStavvieta;
-    int skatijumuSkaits;
-
-
-
-    public void printetAdresi(){
-        System.out.println("Mājas adrese ir: " + ielasNosaukums + " " + majasNumurs);
-    }
-
-    public void printetVisu() {
-        printetAdresi();
-        System.out.println("Māja: " +
-                "stavuSkaits=" + stavuSkaits +
-                ", ieejuSkaits=" + ieejuSkaits +
-                ", istabuSkaits=" + istabuSkaits +
-                ", platiba=" + platiba + "m^2" +
-                ", cena=" + cena +
-                ", irStavvieta=" + irStavvieta +
-               ", cena kvadrātmetrā ir " + cena.cena/platiba +
-                '}');
-    }
-}
+//package clasroomFour;
+//
+//public class House {
+//
+//    String ielasNosaukums;
+//    int majasNumurs;
+//    int stavuSkaits;
+//    int ieejuSkaits;
+//    int istabuSkaits;
+//    double platiba;
+//    Cena cena;
+//    boolean irStavvieta;
+//    int skatijumuSkaits;
+//
+//
+//
+//    public void printetAdresi(){
+//        System.out.println("Mājas adrese ir: " + ielasNosaukums + " " + majasNumurs);
+//    }
+//
+//    public void printetVisu() {
+//        printetAdresi();
+//        System.out.println("Māja: " +
+//                "stavuSkaits=" + stavuSkaits +
+//                ", ieejuSkaits=" + ieejuSkaits +
+//                ", istabuSkaits=" + istabuSkaits +
+//                ", platiba=" + platiba + "m^2" +
+//                ", cena=" + cena +
+//                ", irStavvieta=" + irStavvieta +
+//               ", cena kvadrātmetrā ir " + cena.cena/platiba +
+//                '}');
+//    }
+//}
--- src/main/java/classHomework/Trissturis.java
+++ src/main/java/classHomework/Trissturis.java
@@ -1,47 +1,47 @@
-package classHomework;
-
-public class Trissturis{
-
-    // visu malu garumu mainīgajie(veseli skaitļi) un laukums
-    int malaA, malaB, malaC;
-    double laukums;
-
-    // non-arg konstruktors, kurš pasaka, ka veidojam trīsstūri
-    Trissturis() {
-        System.out.println("Veidoju trīsstūri");
-    }
-
-    //arg konstruktors, kur tiek padotas visas trīs malas un uzreiz aprēķināts trijstūra laukums(hērona formula) un saglabāts laukuma mainīgajā
-
-    Trissturis(int malaA, int malaB, int malaC) {
-        this.malaA = malaA;
-        this.malaB = malaB;
-        this.malaC = malaC;
-    }
-    //laukuma mainīgais, ko izvadīt Main klasē
-    public double laukums() {
-        double pusperimetrs = (malaA + malaB + malaC) / 2;
-        System.out.print("Trīsstūra laukums ir ");
-        return Math.sqrt(pusperimetrs * (pusperimetrs - malaA) * (pusperimetrs - malaB) * (pusperimetrs - malaC));
-    }
-
-    //metode, kas atgriež true/false vai trīsstūris ir vienādmalu
-    public boolean vaiVienadmalu (){
-
-        if (malaA == malaB && malaB== malaC && malaA == malaC){
-            return true;
-        }else {
-            return false;
-        }
-    }
-
-    public boolean vaiVienadsanu () {
-
-        if ((malaA == malaB && malaB != malaC) || (malaB == malaC && malaC !=malaA) || (malaC == malaA && malaA != malaB)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-}
+//package classHomework;
+//
+//public class Trissturis{
+//
+//    // visu malu garumu mainīgajie(veseli skaitļi) un laukums
+//    int malaA, malaB, malaC;
+//    double laukums;
+//
+//    // non-arg konstruktors, kurš pasaka, ka veidojam trīsstūri
+//    Trissturis() {
+//        System.out.println("Veidoju trīsstūri");
+//    }
+//
+//    //arg konstruktors, kur tiek padotas visas trīs malas un uzreiz aprēķināts trijstūra laukums(hērona formula) un saglabāts laukuma mainīgajā
+//
+//    Trissturis(int malaA, int malaB, int malaC) {
+//        this.malaA = malaA;
+//        this.malaB = malaB;
+//        this.malaC = malaC;
+//    }
+//    //laukuma mainīgais, ko izvadīt Main klasē
+//    public double laukums() {
+//        double pusperimetrs = (malaA + malaB + malaC) / 2;
+//        System.out.print("Trīsstūra laukums ir ");
+//        return Math.sqrt(pusperimetrs * (pusperimetrs - malaA) * (pusperimetrs - malaB) * (pusperimetrs - malaC));
+//    }
+//
+//    //metode, kas atgriež true/false vai trīsstūris ir vienādmalu
+//    public boolean vaiVienadmalu (){
+//
+//        if (malaA == malaB && malaB== malaC && malaA == malaC){
+//            return true;
+//        }else {
+//            return false;
+//        }
+//    }
+//
+//    public boolean vaiVienadsanu () {
+//
+//        if ((malaA == malaB && malaB != malaC) || (malaB == malaC && malaC !=malaA) || (malaC == malaA && malaA != malaB)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//}
