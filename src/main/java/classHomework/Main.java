5b69da3b7af494dff8efbb9599ffed281a61ea0f
liva-nazarenko <105019479+liva-nazarenko@users.noreply.github.com>
2022-06-18 17:19:32 +0300

    up

---
+++ src/main/java/classHomework/Main.java
@@ -0,0 +1,32 @@
+package classHomework;
+public class Main {
+    public static void main(String[] args) {
+
+        Trissturis trissturis1 = new Trissturis();
+        trissturis1.malaA = 5;
+        trissturis1.malaB = 7;
+        trissturis1.malaC = 8;
+
+        System.out.println("Trīsstūra laukums ir " + trissturis1.laukums + " m2");// ko man vajadzēja darīt savādāk? Kādēļ izvada 0?
+        System.out.println();
+        System.out.print("Vai trīsstūris ir vienādmalu?: " + trissturis1.vaiVienadmalu());
+        System.out.println();
+        System.out.print("Vai trīsstūris ir vienādsānu?: " + trissturis1.vaiVienadsanu());
+
+        System.out.println();
+        System.out.println();
+
+        Trissturis trissturis2 = new Trissturis(5,6,5);
+        System.out.print(trissturis2.laukums() + " m2");
+        System.out.println();
+
+        System.out.print("Vai trīsstūris ir vienādmalu?: " + trissturis2.vaiVienadmalu());
+
+        System.out.println();
+        System.out.print("Vai trīsstūris ir vienādsānu?: " + trissturis2.vaiVienadsanu());
+
+
+    }
+
+}
+
