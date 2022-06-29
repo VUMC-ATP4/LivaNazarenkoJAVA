45e55a5a704cb1119be2baff8f324a0dd6f6c498
liva-nazarenko <105019479+liva-nazarenko@users.noreply.github.com>
2022-06-29 13:11:27 +0300

    update

--- src/main/java/homework/oop_homework/Customer.java
+++ src/main/java/homework/oop_homework/Customer.java
@@ -2,39 +2,38 @@ package homework.oop_homework;
 
 public class Customer {
 
-    String name;
-    boolean isMember;
-    String memberType;
+    private String name;
+    private boolean member= false;
+    private String memberType;
 
-    public Customer(String name, boolean isMember, String memberType) {
+    public Customer(String name) //boolean member, String memberType)
+    {
         this.name = name;
+//        this.member = member;
+//        this.memberType = memberType;
     }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public boolean isMember() {
+        return member;
     }
-
     public String getMemberType() {
         return memberType;
     }
-
-    public String isMember(){
-        return memberType;
-    }
-
     public void setMemberType(String memberType) {
         this.memberType = memberType;
+    }
+
+    public void setMember(boolean member) {
+        this.member = member;
     }
 
     @Override
     public String toString() {
         return "Customer{" +
                 "name='" + name + '\'' +
-                ", isMember=" + isMember +
+                ", member=" + member +
                 ", memberType='" + memberType + '\'' +
                 '}';
     }
--- src/main/java/homework/oop_homework/Example.java
+++ src/main/java/homework/oop_homework/Example.java
@@ -1,12 +1,64 @@
 package homework.oop_homework;
+import java.util.Date;
+
+
+
 
 public class Example {
-    public static void main(String[] args) {
 
-Customer costumer1 = new Customer("Aija", true, "Premium");
-        Customer costumer2 = new Customer("Jana", true, "Gold");
-        Customer costumer3 = new Customer("Otto", true, "Silver");
-        Customer costumer4 = new Customer("Artis", false, "none");
+        public static void main(String[] args) {
+            System.out.println();
+
+            Customer costumer1 = new Customer("Aija");
+            costumer1.setMember(true);
+            costumer1.setMemberType("Premium");
+
+            Visit visit = new Visit(costumer1, new Date());
+           // System.out.println(visit.toString());
+
+            visit.setProductExpense(5.00);
+            visit.setServiceExpense(15.20);
+            System.out.println(visit);
+
+            System.out.println("Total expense for " + visit.getName() + " = " + visit.getTotalExpense());
+
+            System.out.println();
+
+            Customer costumer2 = new Customer("Jana");
+            costumer2.setMember(true);
+            costumer2.setMemberType("Gold");
+
+            Visit visit2 = new Visit(costumer2, new Date());
+
+            visit2.setProductExpense(5.00);
+            visit2.setServiceExpense(15.20);
+            System.out.println(visit2);
+            System.out.println("Total expense for " + visit2.getName() + " = " + visit2.getTotalExpense());
+
+            System.out.println();
+
+            Customer costumer3 = new Customer("Anna");
+            costumer3.setMember(true);
+            costumer3.setMemberType("Silver");
+
+            Visit visit3 = new Visit(costumer3, new Date());
+
+            visit3.setProductExpense(5.00);
+            visit3.setServiceExpense(15.20);
+            System.out.println(visit3);
+            System.out.println("Total expense for " + visit3.getName() + " = " + visit3.getTotalExpense());
+
+            System.out.println();
+
+            Customer costumer4 = new Customer("Otto");
+            costumer4.setMember(false);
+
+            Visit visit4 = new Visit(costumer4, new Date());
+
+            visit4.setProductExpense(5.00);
+            visit4.setServiceExpense(15.20);
+            System.out.println(visit4);
+            System.out.println("Total expense for " + visit4.getName() + " = " + visit4.getTotalExpense());
+      }
 
     }
-}
